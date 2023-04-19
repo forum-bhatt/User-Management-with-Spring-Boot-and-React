c8193d2e1f8a6485065bf15d9f5805ffaecc1fa8
Forum Bhatt <bhatt.f@northeastern.edu>
2023-04-19 19:03:42 -0400

    Backend and Frontend code

---
+++ Backend/src/main/java/com/crud/backend/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.crud.backend.exception;
+
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not find user with id: " + id);
+
+    }
+}
---
+++ Backend/src/main/java/com/crud/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.crud.backend.repository;
+
+import com.crud.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
